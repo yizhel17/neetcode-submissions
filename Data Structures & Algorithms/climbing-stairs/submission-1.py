c1a9c60dd9035b85fbfed3e8e806c8449069adaf
YizheL. James <yizhel17@uci.edu>
2026-07-17 21:49:16 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # fir, sec = 1, 1 #bottom-up的形式, 由右到左 推进
+
+        # for i in range(n-1): #根据推导的结论, 还要算出n-1个数,所以还需循环n-1次
+        #     temp = fir
+        #     fir = fir + sec
+        #     sec = temp
+        
+        # return fir
+
+        fir = 1 #在第0阶
+        sec = 1 #在第1阶层
+
+        for i in range(2, n + 1):
+            temp = fir
+            fir = sec
+            sec = temp + sec
+        
+        return sec
