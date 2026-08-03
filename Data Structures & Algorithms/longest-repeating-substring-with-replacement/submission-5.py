fd2b9617b65066098c4b88d05d7c424b8be5f514
YizheL. James <yizhel17@uci.edu>
2026-08-03 16:42:49 -0700

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #update every time when we see a larger substring
+        max_l = 0
+        max_t = 0
+        
+        #hash map, track the times the element appears
+        my_map = defaultdict(int)
+
+        #Sliding window (left: slow, right: fast) --- Two pointers
+        l = 0
+        for r in range(len(s)):
+            my_map[s[r]] += 1
+            max_t = max(my_map[s[r]], max_t)
+
+            if (r-l+1) - max_t > k:
+                my_map[s[l]] -= 1
+                l += 1
+            
+            
+            length = (r-l+1)
+            max_l = max(max_l, length) #any window that satisfies the standard must be recorded.
+        
+        return max_l
